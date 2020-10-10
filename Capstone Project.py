5720b2adab4aff4ccd113f7fa844466c77fb5813
ahenkora10 <51715434+ahenkora10@users.noreply.github.com>
2020-10-10 02:54:31 +0100

    Add files via upload

---
+++ Capstone Project.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#  # Notebook for Data Science Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
